0530405bc98dad71a888f51228486b372a3e7ad9
Obi Pranata <obipranata@Obis-MacBook-Air.local>
2022-10-27 22:18:25 +0900

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>UMPTN-MATRIKULASI-LARAVEL</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+  <div class="mb-14">
+    <h1 class="text-3xl text-center pt-14">Ujian Masuk Perusahaan</h1>
+    <h1 class="text-3xl text-center ">Teknologi Nasional</h1>
+    <p class="text-lg text-center mt-3">Pendaftaran Ujian Masuk Perusahaan Teknologi Nasional Dibuka!</p>
+    <p class="text-lg text-center mt-3">Daftar Sekarang</p>
+    <img class="mx-auto mt-14" src="1.png ">
+  </div>
+
+  <div class="place-content-center flex items-center p-8">
+    <img src="2.png">
+    <div>
+      <h1 class="text-3xl mb-3 ">Apa itu UMPTN?</h1>
+      <p class="leading-relaxed text-lg">Kegiatan seleksi yang ditujukan untuk
+        menyaring talenta digital yang siap diserap oleh
+        Perusahaan Teknologi Nasional.</p>
+    </div>
+  </div>
+
+  <h1 class="text-center text-5xl mt-14 mb-5">Program</h1>
+
+  <div class=" grid grid-cols-3 gap-4 place-items-stretch p-8">
+    <div class="max-w-sm rounded overflow-hidden border-solid border-2 shadow-lg p-5">
+      <img class="object-contain h-20 w-96" src="company3.svg" alt="">
+      <h1 class="text-2xl mt-8 text-center">Freedloop</h1>
+      <p class="mt-3 text-center leading-relaxed">
+        Feedloop merupakan sebuah perusahaan startup dalam negeri yang membantu perusahaan untuk bertransformasi menjadi
+        perusahaan digital
+      </p>
+      <div class="flex flex-row justify-center items-center mt-3">
+        <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+          Daftar Sekarang
+        </button>
+      </div>
+    </div>
+    <div class="max-w-sm rounded overflow-hidden border-solid border-2 shadow-lg p-5">
+      <img class="object-contain h-20 w-96" src="company2.png" alt="">
+      <h1 class="text-2xl mt-8 text-center">Digital Skill Rumah Amal Salman</h1>
+      <p class="mt-3 text-center leading-relaxed">
+        Rumah Amal Salman memanggil kamu untuk menjadi bagian dari 9 Juta Talenta Digital di tahun 2030
+      </p>
+      <div class="flex flex-row justify-center items-center mt-3">
+        <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+          Daftar Sekarang
+        </button>
+      </div>
+    </div>
+    <div class="max-w-sm rounded overflow-hidden border-solid border-2 shadow-lg p-5">
+      <img class="object-contain h-20 w-96" src="company1.jpeg" alt="">
+      <h1 class="text-2xl mt-8 text-center">Ujian Masuk FAR Capital</h1>
+      <p class="mt-3 text-center leading-relaxed">Rekrutnen Software Developer untuk FAR Capital Malaysia dengan stack
+        yang diujikan PHP
+        dan Node.js
+      </p>
+      <div class="flex flex-row justify-center items-center mt-3">
+        <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+          Daftar Sekarang
+        </button>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
